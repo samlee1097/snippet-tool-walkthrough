6a4a4e624cc672023bfcc9b5881d5dd6f3112114
Samuel <samlee1097@yahoo.com>
2021-09-22 11:15:38 -0400

    Added snippet code to App.js

---
+++ App.js
@@ -0,0 +1,18 @@
+// {
+// 	"Class Component": {
+// 	"prefix": "cc",
+// 	"body": [
+// 		"import React from 'react';",
+// 		"",
+// 		"function ${1:filename}() {",
+// 		"    return (",
+// 		"",
+// 		"    );",
+// 		"}",
+// 		"",
+// 		"export default ${1:filename};",
+// 		""
+// 		],
+// 	"description": "Class Component Template"
+//     }
+// }
